1412d83dcd16c69eaa216ae1df259d9ee85483b2
Vlad P. Luchian <cstmlcodes@gmail.com>
2023-09-30 20:31:22 +0100

    update: change the deprecated fields and tidy up basic flake.

--- basic/flake.nix
+++ basic/flake.nix
@@ -2,34 +2,27 @@
   description = "A basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url     = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
   flake-utils.lib.eachDefaultSystem (system:
   let
-    overlays = [ ];
-
-    pkgs = import nixpkgs {inherit system overlays;};
-
+    overlays       = [ ];
+    pkgs           = import nixpkgs {inherit system overlays;};
     additionalPkgs = with pkgs; [  ];
-
-    cmake-bin = pkgs.cmake;
-
-    buildPkgs = with pkgs; [ cmake-bin ]; 
+    buildInputs    = with pkgs; [ cmake-bin ]; 
 
     project = pkgs.stdenv.mkDerivation {
-      name = "flake-env";
-      root = self;
-      buildInputs = buildPkgs;
+      inherit buildInputs;
+      name        = "flake-env";
+      root        = self;
       shellInputs = additionalPkgs;
     };
-  in {
-# Used by `nix build` & `nix run` (prod exe)
-  defaultPackage = project;
 
-# Used by `nix develop` (dev shell)
-  devShell = project;
+  in {
+  packages.default  = project; # default package
+  devShells.default = project; # dev shell
   });
 }
